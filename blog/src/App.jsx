886b2a98496f859f23ab500e4a7d8ee4094a202d
lethanhtai <thanhtaile3242@gmail.com>
2024-01-25 18:40:46 +0700

    route

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Dashboard from "./pages/DashBoard.jsx";
+
+import Projects from "./pages/Projects.jsx";
 const App = () => {
     return (
         <>
-            <h1 className="text-3xl text-red-500">abc</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
+            </BrowserRouter>
+            {/* <h1 className="text-3xl text-red-500">abc</h1> */}
         </>
     );
 };
---
+++ blog/src/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <>
+            <div>Aboute</div>
+        </>
+    );
+};
+
+export default About;
