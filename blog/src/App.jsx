b24520f8a43fddc4de8c0a4315bb3eb2fb76b2cc
lethanhtai <thanhtaile3242@gmail.com>
2024-01-25 19:18:10 +0700

    header

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,12 +4,13 @@ import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Dashboard from "./pages/DashBoard.jsx";
-
+import Header from "./components/Header.jsx";
 import Projects from "./pages/Projects.jsx";
 const App = () => {
     return (
         <>
             <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
--- blog/src/pages/Dashboard.jsx
+++ blog/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
     return (
         <>
-            <div>Dashboard</div>
+            <h1>Dashboardaa</h1>
         </>
     );
 };
